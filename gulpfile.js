2e07bdf438547eea961cdb15a6effc3ef0db64b2
Nelson Reis <nelsonfsr@gmail.com>
2015-05-26 19:57:26 +0100

    Remove remote url from options

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp        = require('gulp');
 var deploy      = require('gulp-gh-pages');
 
 var options = { 
-    remoteUrl: "https://github.com/nelsonreis/test-gulp.github.io.git",
     branch: "master" };
 gulp.task('deploy', function () {
   return gulp.src("./dist/**/*")
