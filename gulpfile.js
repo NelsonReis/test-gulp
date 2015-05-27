39658058a48b54ffe97dfddb610fadac07d82b72
Nelson Reis <nelsonfsr@gmail.com>
2015-05-27 12:33:29 +0100

    Include CNAME and .gitignore on deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ var deploy      = require('gulp-gh-pages');
 var options = { 
     branch: "master" };
 gulp.task('deploy', function () {
-  return gulp.src(["dist/**/*.*"])
+  return gulp.src(["dist/**/*.*", "dist/CNAME", "dist/.gitignore"])
     .pipe(deploy(options))
 });
